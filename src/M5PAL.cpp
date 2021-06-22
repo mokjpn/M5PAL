654ab7d77095819c9bebaa65b006eea129f84978
Masafumi Okada <mf@okadajp.org>
2021-06-23 01:01:18 +0900

    serial minor changes

--- src/M5PAL.cpp
+++ src/M5PAL.cpp
@@ -11,11 +11,12 @@
 #define NTPHOST2 "ntp.jst.mfeed.ad.jp"
 #define LINEHOST "notify-api.line.me"
 
-AsciiParser parse_ascii(512);
+AsciiParser parse_ascii(1024);
 Ambient ambient;
 WiFiClientSecure client;
 WiFiClient aclient;
 struct tm timeInfo;
+int failcount = 0;
 
 void wifiConnect() {
   WiFi.disconnect();
@@ -37,6 +38,7 @@ void send(String message) {
   client.setInsecure();
   if (!client.connect(host, 443)) {
     Serial.println("Connection failed");
+    if(++failcount == 1) M5.Power.reset();
     return;
   }
   Serial.println("Connected");
@@ -66,6 +68,7 @@ void send(String message) {
 void setup() {
   M5.begin(false);
   //M5.dis.clear();
+  Serial1.setRxBufferSize(1024);
   Serial.begin(115200);
   Serial1.begin(115200,SERIAL_8N1, 21,25);
   Serial.println("\nStart");
@@ -76,61 +79,73 @@ void setup() {
   Serial.println(s);
 }
 
+double temp=0, hum=0, lumi=0;
+int ambcnt = 0;
+
 void loop() {
   static char message[300], datetime[30];
+  int ch;
   while (Serial1.available()) {
-      int c = Serial1.read();
-      parse_ascii << char_t(c);
-      if (parse_ascii) {
-        getLocalTime(&timeInfo);
-        sprintf(datetime, "Time:%04d/%02d/%02d %02d:%02d:%02d",timeInfo.tm_year+1900, timeInfo.tm_mon+1, timeInfo.tm_mday, timeInfo.tm_hour,timeInfo.tm_min,timeInfo.tm_sec);
-        auto&& pkt = newTwePacket(parse_ascii.get_payload());
-        E_PKT pkt_typ = identify_packet_type(pkt);
-        if (pkt_typ == E_PKT::PKT_PAL) {
-          auto&& pal = refTwePacketPal(pkt);
-              
-          if (pal.u8palpcb == E_PAL_PCB::MAG) {
-            Serial.println("MAG");
-            PalMag obj = pal.get_PalMag();
-            char *status;
-            switch(obj.u8MagStat) {
-              case 0:
-                status = "Open";
-                sprintf(message,"Door open, %s\n", datetime );
-                send(message);
-                break;
-              default:
-                status = "Close";
-            }
-            sprintf(message, "Magnet PAL notify: %s, BAT %d", status, obj.u16Volt);
-            Serial.println(message);
-          } else if (pal.u8palpcb == E_PAL_PCB::AMB) {
-            Serial.println("AMB");
-            PalAmb obj = pal.get_PalAmb();
-            sprintf(message, "Ambient PAL notify: Temp %3.1f, Hum %3.1f, Lux %3d, BAT %d", obj.i16Temp / 100.0 , obj.u16Humd / 100.0, obj.u32Lumi, obj.u16Volt);
-            Serial.println(message);
-            ambient.set(1, obj.i16Temp/100.0);
-            ambient.set(2, obj.u16Humd/100.0);
-            ambient.set(3, (int)(obj.u32Lumi));
-            ambient.set(4, obj.u16Volt);
+    ch = Serial1.read();
+    parse_ascii << char_t(ch);
+    if (parse_ascii) {
+      getLocalTime(&timeInfo);
+      sprintf(datetime, "Time:%04d/%02d/%02d %02d:%02d:%02d",timeInfo.tm_year+1900, timeInfo.tm_mon+1, timeInfo.tm_mday, timeInfo.tm_hour,timeInfo.tm_min,timeInfo.tm_sec);
+      auto&& pkt = newTwePacket(parse_ascii.get_payload());
+      E_PKT pkt_typ = identify_packet_type(pkt);
+      if (pkt_typ == E_PKT::PKT_PAL) {
+        auto&& pal = refTwePacketPal(pkt);
+        if (pal.u8palpcb == E_PAL_PCB::MAG) {
+          Serial.println("MAG");
+          PalMag obj = pal.get_PalMag();
+          char *status;
+          switch(obj.u8MagStat) {
+            case 0:
+              status = "Open";
+              sprintf(message,"Door open, %s\n", datetime );
+              send(message);
+              break;
+            default:
+              status = "Close";
+          }
+          sprintf(message, "Magnet PAL notify: %s, BAT %d", status, obj.u16Volt);
+          Serial.println(message);
+        } else if (pal.u8palpcb == E_PAL_PCB::AMB) {
+          Serial.print("AMB, cnt=");
+          Serial.println(ambcnt);
+          PalAmb obj = pal.get_PalAmb();
+          sprintf(message, "Ambient PAL notify: Temp %3.1f, Hum %3.1f, Lux %3d, BAT %d", obj.i16Temp / 100.0 , obj.u16Humd / 100.0, obj.u32Lumi, obj.u16Volt);
+          Serial.println(message);
+          temp += obj.i16Temp / 100.0;
+          hum += obj.u16Humd / 100.0;
+          lumi += obj.u32Lumi;
+          ambcnt++;
+          if(ambcnt >= 10) {
+            ambient.set(1, temp / 10.0);
+            ambient.set(2, hum / 10.0);
+            ambient.set(3, (int)lumi / 10.0);
+            ambient.set(4, obj.u16Volt);              
+            ambcnt = temp = hum = lumi = 0;
             bool s;
             s = ambient.send();
-            Serial.println(s);
-          } else if (pal.u8palpcb == E_PAL_PCB::MOT) {
-            Serial.println("MOT");
-            PalMot obj = pal.get_PalMot();
-            sprintf(message,"Motion PAL notify: Samples: %d, x[0]: %d, y[0]: %d, z[0]: %d, BAT %d", obj.u8samples, obj.i16X[0], obj.i16Y[0], obj.i16Z[0], obj.u16Volt);
-            Serial.println(message);
-            int sumZ=0;
-            for(int i=0; i<obj.u8samples; i++){
-              sumZ += obj.i16Z[i];
-            }
-            if(sumZ > 0) {
-              sprintf(message, "Milkbox Open, %s\n", datetime);
-              send(message);
-            }
-          } else {
-            Serial.println("UNKNOWN PAL");
+            if(!s)    
+             if(++failcount == 3) M5.Power.reset();
+          }
+        } else if (pal.u8palpcb == E_PAL_PCB::MOT) {
+          Serial.println("MOT");
+          PalMot obj = pal.get_PalMot();
+          sprintf(message,"Motion PAL notify: Samples: %d, x[0]: %d, y[0]: %d, z[0]: %d, BAT %d", obj.u8samples, obj.i16X[0], obj.i16Y[0], obj.i16Z[0], obj.u16Volt);
+          Serial.println(message);
+          int sumZ=0;
+          for(int i=0; i<obj.u8samples; i++){
+            sumZ += obj.i16Z[i];
+          }
+          if(sumZ > 0) {
+            sprintf(message, "Milkbox Open, %s\n", datetime);
+            send(message);
+          }
+        } else {
+          Serial.println("UNKNOWN PAL");
         }
       }
     }
